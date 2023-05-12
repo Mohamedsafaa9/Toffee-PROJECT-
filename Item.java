5ed8165119406e6aca63908df2e8043a3f73e491
Maruom2 <132860044+Maruom2@users.noreply.github.com>
2023-05-12 17:14:05 +0300

    "Added comments  to class Item"

--- Item.java
+++ Item.java
@@ -1,11 +1,25 @@
+/**
+ * The Item class represents a product in the catalog.
+ * It contains information about the product, such as its name, category, description, brand, price, and discount.
+ */
 public class Item {
-    private String name;
-    private String category;
-    private String description;
-    private String brand;
-    private double price;
-    private double discount;
+    private String name; // The name of the item.
+    private String category; // The category of the item.
+    private String description; // The description of the item.
+    private String brand; // The brand of the item.
+    private double price; // The price of the item.
+    private double discount; // The discount percentage of the item.
 
+    /**
+     * Constructor for creating a new Item object.
+     *
+     * @param name        The name of the item.
+     * @param category    The category of the item.
+     * @param description The description of the item.
+     * @param brand       The brand of the item.
+     * @param price       The price of the item.
+     * @param discount    The discount percentage of the item.
+     */
     public Item(String name, String category, String description, String brand, double price, double discount) {
         this.name = name;
         this.category = category;
@@ -15,30 +29,65 @@ public class Item {
         this.discount = discount;
     }
 
+    /**
+     * Getter method for the name of the item.
+     *
+     * @return The name of the item.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Getter method for the category of the item.
+     *
+     * @return The category of the item.
+     */
     public String getCategory() {
         return category;
     }
 
+    /**
+     * Getter method for the description of the item.
+     *
+     * @return The description of the item.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Getter method for the brand of the item.
+     *
+     * @return The brand of the item.
+     */
     public String getBrand() {
         return brand;
     }
 
+    /**
+     * Getter method for the price of the item.
+     *
+     * @return The price of the item.
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Getter method for the discount percentage of the item.
+     *
+     * @return The discount percentage of the item.
+     */
     public double getDiscount() {
         return discount;
     }
 
+    /**
+     * Calculates the discounted price of the item based on the discount percentage.
+     *
+     * @return The discounted price of the item.
+     */
     public double getDiscountedPrice() {
         return price * (1 - discount/100);
     }
